38321a297adf189dcce7fd1ff29e0c68ad60546c
snmtriet <bindsu63@gmail.com>
2024-03-13 16:57:34 +0700

    chore: test connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,42 @@ import {
 } from '@web3modal/wagmi/react'
 import { useEffect } from 'react'
 import { WagmiConfig } from 'wagmi'
-import { polygonMumbai } from 'wagmi/chains'
 import { useAppSelector } from './store'
+import appConfig from './configs/app.config'
 
-const chains = [polygonMumbai]
+// const chains = [polygonMumbai]
+
+// const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ''
+
+// const metadata = {
+//   name: 'Alienz Studio',
+//   description: 'Alienz Studio Description',
+//   url: 'https://web3modal.com',
+//   icons: ['https://avatars.githubusercontent.com/u/37784886'],
+// }
+
+// const wagmiConfig = defaultWagmiConfig({
+//   chains,
+//   projectId,
+//   metadata,
+// })
+
+// createWeb3Modal({
+//   wagmiConfig,
+//   projectId,
+//   chains,
+//   themeMode: 'light',
+// })
+
+const chains = [appConfig.chain]
 
 const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ''
 
 const metadata = {
   name: 'Alienz Studio',
-  description: 'Alienz Studio Description',
-  url: 'https://web3modal.com',
-  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  description: 'Alienz Staking App',
+  url: 'https://alienz-web.vercel.app',
+  icons: [import.meta.env.VITE_DOMAIN + '/img/logo/logo.png'],
 }
 
 const wagmiConfig = defaultWagmiConfig({
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,13 +1,18 @@
+import { polygonMumbai } from 'viem/chains'
+import type { Chain } from 'wagmi'
+
 export type AppConfig = {
   apiPrefix: string
   authenticatedEntryPath: string
   unAuthenticatedEntryPath: string
+  chain: Chain
 }
 
 const appConfig: AppConfig = {
   apiPrefix: import.meta.env.VITE_PUBLIC_API,
   authenticatedEntryPath: '/',
   unAuthenticatedEntryPath: '/sign-in',
+  chain: polygonMumbai,
 }
 
 export default appConfig
